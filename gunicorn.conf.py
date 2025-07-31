0064da7ee91487f4f93157e6a865ec45ad2960cf
ohby88 <142006565+ohby88@users.noreply.github.com>
2025-07-31 23:53:31 +0900

    Create gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,9 @@
+import multiprocessing
+
+# Gunicorn config variables
+loglevel = "info"
+workers = multiprocessing.cpu_count() * 2 + 1
+bind = "0.0.0.0:8000"
+keepalive = 120
+timeout = 120
+worker_class = "gevent"
